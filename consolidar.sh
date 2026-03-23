d02d5795046dd6fc786c0f918411adc1a1427155
felipendelicia <delicia4581@gmail.com>
2026-03-22 21:57:26 -0300

    Quité los echo

--- consolidar.sh
+++ consolidar.sh
@@ -3,24 +3,20 @@ SALIDA=~/EPNro1/salida
 ENTRADA=~/EPNro1/entrada
 PROCESO=~/EPNro1/procesado
 
-echo "FILENAME: $FILENAME"
-
 if [ ! -f "$SALIDA/$FILENAME.txt" ]; then
 	touch "$SALIDA/$FILENAME.txt"
-	echo "Archivo de salida inexistente, creando..."
 fi
 
 while true; do
-	sleep 2
+	sleep 3
 
 	files=$(ls $ENTRADA)
 
 	for item in $files; do
-        	if [ -d "$ENTRADA/$item" ]; then
+        if [ -d "$ENTRADA/$item" ]; then
 			continue
 		fi
-		echo "Archivo añadido $item"
-        	cat "$ENTRADA/$item" >> "$SALIDA/$FILENAME.txt"
-        	mv "$ENTRADA/$item" "$PROCESO/"
+        cat "$ENTRADA/$item" >> "$SALIDA/$FILENAME.txt"
+        mv "$ENTRADA/$item" "$PROCESO/"
 	done
 done
